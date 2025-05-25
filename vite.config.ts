60526c034f9bcb2bf764211c363dcd0e1b448fd4
Goose <marvin.goosesolutions@gmail.com>
2025-05-25 16:16:56 -0500

    Ensure vercel.json routing fix in root

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // 👈 Ensures correct route resolution
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true // 👈 Handles local dev routing too
-  }
-})
+{
+  "rewrites": [
+    { "source": "/(.*)", "destination": "/" }
+  ]
+}
