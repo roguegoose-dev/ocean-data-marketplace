fe0bd76c87a8568f816da2b07c96a81406b7c145
roguegoose-dev <89866155+roguegoose-dev@users.noreply.github.com>
2025-05-25 16:27:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-{
-  "rewrites": [
-    { "source": "/(.*)", "destination": "/" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
+})
