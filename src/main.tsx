cc8d44d33c7be645dbb37f9b417f1cbb1e5b734f
roguegoose-dev <89866155+roguegoose-dev@users.noreply.github.com>
2025-05-25 16:29:19 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App'
 import Marketplace from './Marketplace'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/marketplace" element={<Marketplace />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
