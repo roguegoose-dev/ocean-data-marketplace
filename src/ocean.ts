8b01be4cd6e4c74b79e985e6bd4431747f900c6a
Goose <marvin.goosesolutions@gmail.com>
2025-05-26 11:04:33 -0500

    Remove ocean.ts and move Ocean logic to Netlify backend

--- src/ocean.ts
+++
@@ -1,7 +0,0 @@
-import { Ocean, ConfigHelper } from '@oceanprotocol/lib'
-
-export async function getOceanInstance() {
-  const config = new ConfigHelper().getConfig('polygon') // or 'mumbai'
-  const ocean = await Ocean.getInstance(config)
-  return ocean
-}
